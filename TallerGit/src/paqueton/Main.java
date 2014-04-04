aa812ab0d694cf346b99b00fb96fdf8dc16f3f3b
Githublorens <alelorens@gmail.com>
2014-04-04 10:56:22 -0400

    actividad obligatoria y fome
    0sdfg

--- TallerGit/src/paqueton/Main.java
+++ TallerGit/src/paqueton/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Segundo comit");
-		System.out.println("Linea modificada para el paquete 'paqueton' por red");
+		System.out.println("Linea modificada para el paquete 'paqueton' por red parte 2");
 	}
 
 }
